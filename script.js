65fd89d117b3324d1c7d7d1162e7e11bcb2fb500
OrcaOfTheAzureSea <robert_k_williamson@hotmail.co.uk>
2023-06-11 12:31:01 +0100

    adding konami code

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
+// a key map of allowed keys
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length) {
+      activateCheats();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+});
+
+function activateCheats() {
+
+  var audio = new Audio("./music/Merry Go Round of Life - Howl's Moving Castle (Joe Hisaishi).mp3");
+  audio.play();
+
+  var llamaImage = document.getElementById("llama");
+  llamaImage.hidden = false;
+  var content = document.getElementById("wrapper");
+  content.hidden = true;
+}
+
+
 function imageClick(e){
+  // Grab the modal elements needed
   var modal = document.getElementById("myModal");
   var modalImg = document.getElementById("img01");
-  var captionText = document.getElementById("caption");
 
   modal.style.display = "block";
+  //e is the event that caused the click so currentTarget is the image clicked 
   modalImg.src = e.currentTarget.src;
-  captionText.innerHTML = this.alt;
 }
 
 function imageClose(){
